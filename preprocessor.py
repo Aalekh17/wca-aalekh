7f4df4c3e9afc111a2787c12642ae968e2f6eff3
Aalekh17 <aalekhvishwakarma17@gmail.com>
2026-03-02 12:21:56 +0530

    Fix regex pattern syntax in preprocess function
    resolving regex pattern problem

--- preprocessor.py
+++ preprocessor.py
@@ -3,7 +3,7 @@ import re
 import numpy as np
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
@@ -16,7 +16,7 @@ def preprocess(data):
     users = []
     messages = []
     for message in df['user_message']:
-        entry = re.split('([\w\W]+?):\s', message)
+        entry = re.split(r'([\w\W]+?):\s', message)
         if entry[1:]:
             users.append(entry[1])
             messages.append(entry[2])
