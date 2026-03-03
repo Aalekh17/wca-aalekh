f5f5e7e3d9a65b45fe0a408c5d29183528c88ecb
Aalekh17 <aalekhvishwakarma17@gmail.com>
2026-03-03 13:21:38 +0530

    Remove Google Analytics script from app.py
    Removed Google Analytics script injection from app.py.

--- app.py
+++ app.py
@@ -4,19 +4,15 @@ import helper
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-GA_SCRIPT = """
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_TRACKING_ID"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QNHC9ESRHD"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
+
   gtag('config', 'G-QNHC9ESRHD');
 </script>
-"""
-
-# Inject Google Analytics script invisibly
-st.components.v1.html(GA_SCRIPT, height=1, width=1)
 
 
 st.sidebar.title('Whatsapp Chat Analyser')
